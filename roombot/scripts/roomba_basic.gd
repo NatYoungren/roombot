0c0e28fad738709098f7c5682efb0d8dcb705dd1
NatYoungren <natahyoungren@gmail.com>
2026-01-19 17:02:02 -0500

    Updated roomba with cleaning code

--- roombot/scripts/roomba_basic.gd
+++ roombot/scripts/roomba_basic.gd
@@ -2,6 +2,9 @@ class_name RoombaBasic extends CharacterBody2D
 
 @onready var bumper_area: Area2D = $bumper_area
 
+@onready var cleaner: Cleaner = $Cleaner # Cleaning AoE
+
+
 ## Max speed of the roomba.
 var top_speed: float = 100.0
 
@@ -14,6 +17,15 @@ var turn_speed: float = PI
 ## Angle (radians) that roomba is currently turning towards.
 ## If null, roomba is moving forward.
 var target_angle = null 
+
+
+var prev_clean_position: Vector2 = Vector2.ZERO
+var clean_timer: Timer
+var clean_distance: float = 2.0
+
+
+var filth_cleaned: float = 0.0
+
 
 
 func _process(delta: float) -> void:
@@ -36,6 +48,13 @@ func _process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.from_angle(rotation) * top_speed, accel * delta)
 	
+	# if Input.is_action_just_pressed("ui_accept"):
+	# 	print("Roomba position: ", global_position, " rotation: ", rotation)
+	if position.distance_to(prev_clean_position) >= clean_distance:
+		prev_clean_position = position
+		clean_filth()
+	# cleaner.clean_filth(State.filth_layer)
+
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
@@ -43,3 +62,18 @@ func _physics_process(_delta: float) -> void:
 # Returns true if the bumper area is colliding with anything.
 func _bumper_check() -> bool:
 	return not bumper_area.get_overlapping_bodies().is_empty()
+
+
+func clean_filth() -> void:
+	filth_cleaned += cleaner.clean_filth(State.filth_layer)
+	clean_timer.start()
+	print(filth_cleaned)
+
+
+func _ready() -> void:
+	clean_timer = Timer.new()
+	clean_timer.wait_time = 0.2
+	clean_timer.one_shot = false
+	clean_timer.autostart = true
+	clean_timer.timeout.connect(clean_filth)
+	add_child(clean_timer)
