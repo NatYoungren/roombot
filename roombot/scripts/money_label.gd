8a4ba1543522b8584cfafe5d78bedd3147327a93
NatYoungren <natahyoungren@gmail.com>
2026-01-22 12:42:17 -0500

    Makes more sense to just put the money updating on the label

--- roombot/scripts/money_label.gd
+++ roombot/scripts/money_label.gd
@@ -1,10 +1,8 @@
-extends Node2D
-
-@onready var label: Label = $Label
+extends Label
 
 func _ready() -> void:
 	State.money_updated.connect(update_money_label)
 	update_money_label(State.money)
 
 func update_money_label(value: float) -> void:
-	label.text = "$" + str(int(value))
+	text = "$" + str(int(value))
